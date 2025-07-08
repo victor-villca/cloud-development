694a0bea0dcf14d9cf6617f223e0f22affdde604
victor-fundacion <victor.villca@fundacion-jala.org>
2025-07-08 19:11:45 -0400

    firebase analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {getAnalytics} from "firebase/analytics"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,6 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
